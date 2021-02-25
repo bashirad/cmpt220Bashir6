b0b0e52620f68d5b6d7b7df370449aad35af9b42
Bashir Dahir <bashir.dahir1@marist.edu>
2021-02-25 18:50:34 -0500

    second commit

--- HereTesting.java
+++ HereTesting.java
@@ -1,5 +1,6 @@
 public class HereTesting {
     public static void main(String[] args) {
         System.out.println("We are testing java!"); 
+        System.out.println("I also testing this!");
     }
 }
