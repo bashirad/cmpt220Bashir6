e63b4a0a57d30d707787fa866f2bbdf0f84a9065
Bashir Dahir <bashir.dahir1@marist.edu>
2021-03-05 12:18:04 -0500

    first question done

--- hw01/ComputeDiscriminant.java
+++ hw01/ComputeDiscriminant.java
@@ -1,33 +1,25 @@
 package hw01;
-/* This program computes the discriminant of a quadratical equation
-    The user inputs the appropriate information and this program takes the 
-    the info to compute the discriminant of the equation.
+/* This program computes the discriminant of the following quadratical equation 3x
+2 + 4x + 5 = 0
 */
 
-import java.util.Scanner; //Explicit import
+
 
 public class ComputeDiscriminant {
 
     /* main method*/
     public static void main(String[] orgs) {
 
-        // getting the coefficients of the quadratic equation from the user
-        System.out.println("Write your quadratic equation in this form ax2 + bx + c = 0");
-        System.out.print("Enter your a: ");
-        Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
+        // getting the coefficients of the quadratic equation
+        int a = 3;
+        int b = 4;
+        int c = 5;
 
-        System.out.print("Enter your b: ");
-        int b = input.nextInt();
-
-        System.out.print("Enter your c: ");
-        int c = input.nextInt();
-        
         // Calculating the discriminant
         int discriminant = ((b*b) - 4*a*c);
 
         //printing the result
-        System.out.println("The discriminant of the equation " + a + "x2 + " + b + "x + " + c + " = 0 is: " + discriminant);
+        System.out.println("The discriminant of the quadratical equation 3x2 + 4x + 5 = 0 is: " + discriminant);
         System.out.println("The program ends here");
 
     }
