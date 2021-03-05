614f4273944439bac2a8f855eb305a86e1e72f6e
Bashir Dahir <bashir.dahir1@marist.edu>
2021-03-05 12:38:02 -0500

    third question done

---
+++ hw01/DisplayPattern.java
@@ -0,0 +1,15 @@
+package hw01;
+
+public class DisplayPattern {
+
+    //public method
+    public static void main(String[] orgs) {
+
+        System.out.println("* ***** *****");
+        System.out.println("* *   * *   *");
+        System.out.println("* *   * *   *");
+        System.out.println("* *   * *   *");
+        System.out.println("* ***** *****");
+    }
+    
+}
