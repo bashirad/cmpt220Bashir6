c30cef9bd137bd57722dedf5dd5e0a69b35daac3
Bashir Dahir <bashir.dahir1@marist.edu>
2021-03-05 12:29:38 -0500

    second question

---
+++ hw01/ComputeAcceleration.java
@@ -0,0 +1,22 @@
+package hw01;
+
+//This program computes the acceleration based on the difference of velocity over time
+public class ComputeAcceleration {
+
+    //main method
+    public static void main(String[] orgs) {
+
+        //declaring variables
+        double initialVelocity = 5.6;
+        double finalVelocity = 10.5;
+        double time = 0.5;
+
+        //calculating the acceleration
+        double acceleration = ((finalVelocity - initialVelocity)/time);
+
+        //displaying the result
+        System.out.println("The acceleration is " + acceleration + );
+        System.out.println("based on initial velocity of 5.6 m/s and a final velocity of 10.5 m/s in 0.5 s span of time");
+    }
+    
+}
