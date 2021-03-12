c0854cb90aa9498bb1bc9ec0ab01e5e5308a25a5
Bashir Dahir <bashir.dahir1@marist.edu>
2021-03-12 12:28:21 -0500

    second problem's solution

---
+++ hw02/OneDimensionalMotion.java
@@ -0,0 +1,23 @@
+package hw02;
+    //importing Scanner
+import java.util.Scanner; 
+
+public class OneDimensionalMotion {
+    public static void main(String[] args) {
+
+        // Create a Scanner
+    Scanner input = new Scanner(System.in);
+
+        //prompting the user to enter the travel time
+    System.out.print("Enter the ball travel time in seconds: ");
+    double time = input.nextDouble();
+    input.close();
+
+        // calculating the height of the building
+    double height = (9.8 / 2) * (time * time);
+
+        //print results
+    System.out.println("The height of the building is " + (int)(height * 1000) / 1000.0);
+
+    }
+}
