1926b708152d4786ca700c82bc323c6858ad5e7f
Bashir Dahir <bashir.dahir1@marist.edu>
2021-04-16 23:27:42 -0400

    m

---
+++ hw06/DigitOccurance.java
@@ -0,0 +1,10 @@
+public class DigitOccurance {
+    public static void main(String[] args) {
+        
+    }
+
+    //method to count how many times a digit occurs in a string
+    public static int [] count(String s){
+
+    }
+}
