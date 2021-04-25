95e18ae8de894891c0f65b490d01b4e82c50fa17
Juan Arias <juan.arias@marist.edu>
2021-04-24 23:01:47 -0400

    Feedback

--- hw07/CenterOfTriangle.java
+++ hw07/CenterOfTriangle.java
@@ -6,39 +6,25 @@ public class CenterOfTriangle {
         System.out.print("Enter x1, y1, x2, y2, x3, y3: ");
         Scanner input = new Scanner(System.in);
         //read the three points
-        Points point1 = new Points(input.nextDouble(), input.nextDouble());
-        Points point2 = new Points(input.nextDouble(), input.nextDouble());
-        Points point3 = new Points(input.nextDouble(), input.nextDouble());
+        Point point1 = new Point(input.nextDouble(), input.nextDouble());
+        Point point2 = new Point(input.nextDouble(), input.nextDouble());
+        Point point3 = new Point(input.nextDouble(), input.nextDouble());
         input.close();
         //get center point
-        Points center = Points.getCenterPoint(point1, point2, point3);
+        Point center = getCenterPoint(point1, point2, point3);
         System.out.println("The Center point is at (" + center.getX() + ", " + center.getY() + ")");
     }
-}
 
-class Points {
-    private double x;
-    private double y;
-    //construct a point with a specific x and y
-    public Points(double x, double y){
-        this.x = x;
-        this.y = y;
-    }
-    public double getX(){
-        return x;
-    }
-    public double getY(){
-        return y;
-    }
-    public static Points getCenterPoint(Points p1, Points p2, Points p3){
+    public static Point getCenterPoint(Point p1, Point p2, Point p3){
         double centerX;
         double centerY;
-        Points centre;
+        Point centre;
         centerX = (( (p1.getX()) + (p2.getX()) + (p3.getX()) )/3);
         centerY = (( (p1.getY()) + (p2.getY()) + (p3.getY()) )/3); 
-        centre =  new Points(centerX, centerY);
+        centre =  new Point(centerX, centerY);
         return centre;
         
-    }
+    }   
 }
+
 
