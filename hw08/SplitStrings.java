bb82cb639d4d87c9344b051fe9f387c5881b87a4
Bashir Dahir <bashir.dahir1@marist.edu>
2021-04-30 15:55:29 -0400

    ...

--- hw08/SplitStrings.java
+++ hw08/SplitStrings.java
@@ -1,24 +1,26 @@
 public class SplitStrings {
     public static void main(String[] args) {
-        String message = "b3!3h@kjh5 jh#h4@g1khj 3k k2  gy$g1 kj!4j hg 23@jhh hg1$j2hg h";
-        ArrayList newString = new ArrayList.split(message, ! $ );
+        //String message = "b3!3h@kjh5 jh#h4@g1khj 3k k2  gy$g1 kj!4j hg 23@jhh hg1$j2hg h";
+        //ArrayList newString = new ArrayList.split(message, ! $ );
     }
-}
 
-class ArrayList {
 
-    public static ArrayList<string> split(String s, String delimiters){
+
+
+    public static ArrayList<String> split(String s, String delimiters){
         String splittedMessage = "";
-        for (i = 0; i <= ArrayList.length(); i++){
-            if (list.get(i) == delimiters) {
-                splittedMessage += " ,"; 
-                splittedMessage += list.remove(i);
+        for (int i = 0; i <= s.length(); i++){
+            char character = s.charAt(i);
+            if (character == ) {
+                splittedMessage += ArrayList.add(i, " ,"); 
+                splittedMessage += ArrayList.remove(i);
             }
             else {
-                splittedMessage += list.get(i);
+                splittedMessage += ArrayList.get(i);
             }
 
             }
         return splittedMessage;
     }
+
 }
