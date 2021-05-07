b7fe5c30e6d319945c832188ad8f4beac6327751
Bashir Dahir <bashir.dahir1@marist.edu>
2021-05-07 17:28:04 -0400

    ...

--- hw09/ArrayIndexOutOfBoundsException.java
+++ hw09/ArrayIndexOutOfBoundsException.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class ArrayIndexOutOfBoundsException {
     public static void main(String[] args) {
+
         //build an array with one hundred random intergers
         int[] randomArray = new int[100]; 
         for (int j = 0; j < randomArray.length; j++) {
             randomArray[j] = (int) (Math.random() * 100);
         }
+
+        //create a scanner
+        Scanner input = new Scanner(System.in);
+        //promt the user to enter the index of the array
+        System.out.print("Enter the index of the array: ");
+        input.close();
+        
+        try {
+            int theIndex = input.nextInt();
+            System.out.println("In the array, the element at " + theIndex + " is " + randomArray[theIndex]);
+        }
+        catch (IndexOutOfBoundsException ex) {
+            System.out.println(ex );
+        }
+
     }
 }
