39245a75829ec5041e08efec672ced061fb24b03
Bashir Dahir <bashir.dahir1@marist.edu>
2021-05-07 17:07:02 -0400

    ...

--- hw09/ArrayIndexOutOfBoundsException.java
+++ hw09/ArrayIndexOutOfBoundsException.java
@@ -1,3 +1,9 @@
 public class ArrayIndexOutOfBoundsException {
-    
+    public static void main(String[] args) {
+        //build an array with one hundred random intergers
+        int[] randomArray = new int[100]; 
+        for (int j = 0; j < randomArray.length; j++) {
+            randomArray[j] = (int) (Math.random() * 100);
+        }
+    }
 }
