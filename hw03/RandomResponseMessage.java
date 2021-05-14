d95aa4caa37e672e034f4f799855913cbe445080
Bashir Dahir <bashir.dahir1@marist.edu>
2021-05-14 16:37:00 -0400

    ...

--- hw03/RandomResponseMessage.java
+++ hw03/RandomResponseMessage.java
@@ -1,8 +1,6 @@
-//package hw03;
+package hw03;
 
-import java.util.Scanner; 
-import java.util.Arrays; 
-import java.util.List; 
+import java.util.Scanner;
 
 public class RandomResponseMessage {
   public static void main(String[] args) {
