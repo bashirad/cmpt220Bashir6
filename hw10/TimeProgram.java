b897a5586df910c19e441ba790dc629e9b3164d2
Bashir Dahir <bashir.dahir1@marist.edu>
2021-05-14 21:36:34 -0400

    ...

--- hw10/TimeProgram.java
+++ hw10/TimeProgram.java
@@ -1,8 +1,9 @@
 package hw10;
+
 //import scanner
 import java.util.Scanner;
 
-public class TimeProgram  {
+public class TimeProgram {
     public static void main(String[] args) {
         //create a scanner
         Scanner input = new Scanner(System.in);
@@ -17,45 +18,53 @@ public class TimeProgram  {
         long realMinute = Long.parseLong(minute);
         long realSecond = Long.parseLong(second);
 
-        System.out.println(time1.getHour() + " hours " + time1.getMinute() + " minutes " + time1.getSecond()+ " seconds ");
-        System.out.println("Elapsed seconds in time1: " + realHour*3600 + realMinute*60 + realSecond);
-        System.out.print("Enter time2 (elapsed time): ");
-        Time time2 = new Time(input.nextLong());
-        System.out.println(time2.getHour() + " hours " + time2.getMinute() + " minutes " + time2.getSecond()+ " seconds ");
-        System.out.println("Elapsed seconds in time1: " + time2);
-        
-    }
+        Time time = new Time(realHour * 3600 + realMinute + realSecond);
+        Time time2 = new Time();
+        System.out.println(time.getHour() + " hours " + time.getMinute() + " minutes " + time.getSecond() + " seconds");
+        System.out.println("Elapsed seconds in time1: " + realHour * 3600 + realMinute + realSecond);
 
+        System.out.println(time2.getHour() + " hours " + time2.getMinute() + " minutes " + time2.getSecond() + " seconds");
+        System.out.print("Enter time2 (Elapsed Time): ");
+        long elapTime2 = input.nextLong();
+        time2.setTime(elapTime2);
+        System.out.println(time2.getHour() + " hours " + time2.getMinute() + " minutes " + time2.getSecond() + " seconds");
+        System.out.print("Elapsed Time in time2 is: " + elapTime2);
+
+    }
+}
 
 class Time {
-    private long time;
+    private long mTime;
 
     public Time() {
-        time = System.currentTimeMillis();
+        mTime = System.currentTimeMillis();
     }
 
     public Time(long time) {
-        this.time = time;
+        mTime = time;
     }
-
-    public Time(long hour, long minute, long second) {
+    public Time(int hour, int minute, int second) {
     }
 
     public void setTime(long elapsedTime) {
-        elapsedTime = System.currentTimeMillis() / 1000;
+        mTime = elapsedTime;
     }
 
 
-    public long getHour() {
-        return (time / (1000 * 60 * 60)) % 24;
+    public int getHour() {
+        return (int)(mTime / (1000 * 60 * 60)) % 24;
     }
 
-    public long getMinute() {
-        return (time / (1000 * 60)) % 60;
+    public int getMinute() {
+        return (int)(mTime / (1000 * 60)) % 60;
     }
 
-    public long getSecond() {
-        return (time / 1000) % 60;
+    public int getSecond() {
+        return (int)(mTime / 1000) % 60;
     }
+
 }
+
+ class Comparison implements Comparable<Time> {
+    return  this.getHour().compareTo(Time.getHour);    
 }
