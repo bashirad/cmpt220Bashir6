0681f107a5167c27bd90ccb802f5ea72e959008e
Bashir Dahir <bashir.dahir1@marist.edu>
2021-05-21 16:29:36 -0400

    ...

--- hw06/DigitOccurance.java
+++ hw06/DigitOccurance.java
@@ -1,8 +1,19 @@
 package hw06;
+
+import java.util.Scanner;
+
 public class DigitOccurance {
     public static void main(String[] args) {
-        
+        //Create a scanner
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String stringFromUser = input.nextLine();
+        System.out.print("Digit 1 occurs " + "times.");
+        input.close();
     }
 
+    public static int[] count(String s) {
+
+    }
 
 }
