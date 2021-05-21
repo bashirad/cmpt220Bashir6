1514e1916666feaad262dfd2d043cb9b10b3086d
Bashir Dahir <bashir.dahir1@marist.edu>
2021-05-21 13:14:34 -0400

    ...

--- hw11/KnapsackProblemTwo.java
+++ hw11/KnapsackProblemTwo.java
@@ -1,8 +1,10 @@
 package hw11;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class KnapsackProblemTwo {
+public class KnapsackProblemTwo extends Comparators{
     public static void main(String[] args) {
         //create a scanner
         Scanner input = new Scanner(System.in);
@@ -10,25 +12,28 @@ public class KnapsackProblemTwo {
         //read the scanner
         System.out.print("Enter the number of items: ");
         int numberOfItems = input.nextInt();
-        System.out.print("Enter the weights for each item: ");
+        System.out.print("Enter the weight for each item: ");
         double [] weightOfItems = new double[numberOfItems + 1];
         for (int i = 0; i < numberOfItems; i++) {
             weightOfItems [i] = input.nextDouble();
-            System.out.print(weightOfItems[i]);
         }
         System.out.print("Enter the weight limit for the bag: ");
         double bagWeightLimit = input.nextDouble();
         System.out.print("The maximum weight of the items placed in the bag is " + m(numberOfItems, bagWeightLimit, weightOfItems));
+        input.close();
     }
 
-    public static ArrayList<Integer> m(int i, double wieghtLimit, double [] weights) {
+    public static ArrayList<Integer> m(int i, double wieghtLimit, double [] w) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
         //Base case
         if (i == 0 || wieghtLimit == 0) {
-            return 0.0;
+            return null;
         }
-        if (weights[i] > wieghtLimit) {
-            return m(i - 1, wieghtLimit, weights);
+        if (w[i] > wieghtLimit) {
+            return m(i - 1, wieghtLimit, w);
         }
-            return Math.max(m(i - 1, wieghtLimit, weights), weights[i] + m(i - 1, wieghtLimit - weights[i], weights));
+            answer = answer.add(Collections.max(m(i - 1, wieghtLimit, w), m(i - 1, wieghtLimit, w).set(i - 1, (int)w[i])));
+            wieghtLimit = wieghtLimit - w[i];
+            return answer;
     }
 }
